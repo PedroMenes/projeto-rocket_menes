34f73ad3f9438dcd672d9a9f426b9129626a4cab
PedroMenes <pereiramouton@gmail.com>
2025-04-20 23:09:55 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-    const img = document.querySelector("profile img")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
-    if(html.classList.contains("light")) {
-      img.setAttribute("src", "./assets/Rogueicon.jfif")  
-    } else {
-        img.setAttribute("src", "./assets/Gambit.jfif")
-    } 
-
-   
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
